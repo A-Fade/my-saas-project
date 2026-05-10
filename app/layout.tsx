c8a229efb9f12e97fe4bdd3f976dd5cde625f9c4
afzal <youremail@gmail.com>
2026-05-10 12:52:46 +0530

    Fixed layout and responsive sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,6 @@ import "./globals.css";
 
 import { Toaster } from "react-hot-toast";
 
-import Sidebar from "@/app/components/Sidebar";
-import Topbar from "@/app/components/Topbar";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -33,30 +30,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
       <body className="bg-gray-100">
-
+        {/* TOAST */}
         <Toaster position="top-right" />
 
-        <div className="flex min-h-screen">
-
-          {/* DESKTOP SIDEBAR */}
-          <div className="hidden md:block fixed left-0 top-0 h-screen w-64 z-40">
-            <Sidebar />
-          </div>
-
-          {/* MAIN CONTENT */}
-          <div className="flex-1 md:ml-64">
-
-            {/* MOBILE TOPBAR */}
-            <div className="md:hidden sticky top-0 z-50">
-              <Topbar />
-            </div>
-
-            {children}
-
-          </div>
-
-        </div>
-
+        {/* PAGES */}
+        {children}
       </body>
     </html>
   );
